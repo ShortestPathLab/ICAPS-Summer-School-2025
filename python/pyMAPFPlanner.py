9c31b406dd0e544eb320269af0ae6044772cc06c
Kevin Zheng <kaiwen530@gmail.com>
2025-11-03 21:11:33 +1100

    Disable logging

--- python/pyMAPFPlanner.py
+++ python/pyMAPFPlanner.py
@@ -31,7 +31,7 @@ from piglet.lib_piglet.output.trace_output import trace_output
 
 # 0=Action.FW, 1=Action.CR, 2=Action.CCR, 3=Action.W
 
-LOG_ENABLED = True
+LOG_ENABLED = False
 
 
 class pyMAPFPlanner:
