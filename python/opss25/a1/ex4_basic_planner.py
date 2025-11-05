60f946f2b27695ea38e1b34021440eadd510e73c
YueZhang-studyuse <brynn.yuezhang@gmail.com>
2025-11-05 15:09:00 +1100

    small test cases for a1 and a2

--- python/opss25/a1/ex4_basic_planner.py
+++ python/opss25/a1/ex4_basic_planner.py
@@ -36,33 +36,11 @@ def naive_planner(domain: robotrunners, use_with_startkit: BindStartKit):
     def plan(
         env: MAPF.SharedEnvironment, paths: list[list], last_did_error: bool = False
     ):
-        if last_did_error:
-            raise RuntimeError("Error: Last action did not successfully commit.")
-        # If there's ANY paths that's not planned, plan entire thing
-        # Should return new paths
+        #plan for all agents if any agent is unplanned
         if any(not p for p in paths):
             return [run_search(env, i) for i, _ in enumerate(paths)]
         else:
             return paths
-
-    return plan
-
-
-def plan_unplanned_only_planner(domain: robotrunners, use_with_startkit: BindStartKit):
-    """
-    Creates a planner that plans for only agents with missing paths.
-    This planner does not replan on unsuccessful commits.
-    """
-
-    engine = create_search(domain)
-    run_search = use_with_startkit(engine)
-
-    def plan(
-        env: MAPF.SharedEnvironment, paths: list[list], last_did_error: bool = False
-    ):
-        if last_did_error:
-            raise RuntimeError("Error: Last action did not successfully commit.")
-        return [p if p else run_search(env, i) for i, p in enumerate(paths)]
 
     return plan
 
